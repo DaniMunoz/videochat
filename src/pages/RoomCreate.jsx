48951e40bb7bab0943cd23e56cbc56df72fafee0
Dani <danielmunozgil@gmail.com>
2023-04-30 22:07:18 +0200

    routes updated

--- src/pages/RoomCreate.jsx
+++ src/pages/RoomCreate.jsx
@@ -12,8 +12,8 @@ function RoomCreatePage() {
         console.log(data.room);
         //window.location.replace("/room-join?roomId=" + data.room);
         //navigate(`/room-join/${data.room}`);
-        //navigate(`/room/room-join/${data.room}`);
-        navigate(`/room-join/${data.room}`);
+        navigate(`/room/room-join/${data.room}`);
+        //navigate(`/room-join/${data.room}`);
       });
   }
 
