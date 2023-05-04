f7a9be9cec55a45e58e7dac5c24fbadad6aff4d5
Dani <danielmunozgil@gmail.com>
2023-05-04 18:49:33 +0200

    Now it works properly

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import RootLayout from './pages/Root';
 import ErrorPage from './pages/Error';
 import RoomCreatePage from './pages/RoomCreate';
 import RoomJoinPage from './pages/RoomJoin';
-import RoomJoin2Page from './pages/RoomJoin2';
 
 const router = createBrowserRouter([
   { path: '/', 
@@ -21,7 +20,6 @@ const router = createBrowserRouter([
     basename: "/room", 
     children: [
       { path: '/room/room-join/:roomId', element:<RoomJoinPage/> },
-      { path: '/room/room-join2/:roomId', element:<RoomJoin2Page/> },
     ]
   },
 ]);
