f7a17fbbfa6b093063b8999361c3ffc135131ee9
Dani <danielmunozgil@gmail.com>
2023-05-04 16:53:59 +0200

    testing an alternate way on RoomJoin2

--- src/pages/RoomJoin2.jsx
+++ src/pages/RoomJoin2.jsx
@@ -44,23 +44,23 @@ export default function RoomJoinPage() {
       .then((stream) => {
         myStream.current = stream;
         addVideoStream(myVideo, myStream.current);
-
-        //////////////////////////////////////
-        myPeer.on("call", (call) => {
-          call.answer(myStream.current);
-          const video = document.createElement("video");
-          call.on("stream", (userVideoStream) => {
-            console.log("call.on.stream 1");
-            addVideoStream(video, userVideoStream);
-          });
-        });
-
-        socket.on("user-connected", (userId) => {
-          console.log("User connected 1: " + userId);
-          connectToNewUser(userId, myStream.current);
-        });
-        /////////////////////////////////////////
       });
+
+    //////////////////////////////////////
+    myPeer.on("call", (call) => {
+      call.answer(myStream.current);
+      const video = document.createElement("video");
+      call.on("stream", (userVideoStream) => {
+        console.log("call.on.stream 1");
+        addVideoStream(video, userVideoStream);
+      });
+    });
+
+    socket.on("user-connected", (userId) => {
+      console.log("User connected 1: " + userId);
+      connectToNewUser(userId, myStream.current);
+    });
+    /////////////////////////////////////////
 
     myPeer.on("open", (id) => {
       socket.emit("join-room", ROOM_ID, id);
@@ -68,7 +68,7 @@ export default function RoomJoinPage() {
 
     socket.on("user-connected", (userId) => {
       console.log("User connected 2: " + userId);
-      connectToNewUser(userId, myStream.current);
+      //connectToNewUser(userId, myStream.current);
     });
 
     socket.on("user-disconnected", (userId) => {
